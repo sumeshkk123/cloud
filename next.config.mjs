8f438ddc328b2492ca059e09af95852fbf284682
bpsumesh <97143552+bpsumesh@users.noreply.github.com>
2026-02-03 15:19:45 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  transpilePackages: ['sanitize-html'],
   // Enable Turbo mode for faster development
   experimental: {
     turbo: {
